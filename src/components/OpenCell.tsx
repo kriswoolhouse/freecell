0f4c9b0084781aa1ba6618290204b5e262ed2bfc
Kristian Woolhouse <kwoolhouse@wonde.com>
2025-09-22 00:45:03 +0100

    Component for basic OpenCell

--- src/components/OpenCell.tsx
+++ src/components/OpenCell.tsx
@@ -1,13 +1,5 @@
-interface OpenCellProps {
-  id: string;
-  type: string;
-}
-
-export default function OpenCell({ id, type }: OpenCellProps) {
+export default function OpenCell() {
   return (
-    <div
-      data-test={`${type}-${id}`}
-      className="aspect-[5/7] border-2 border-blue-100/60 bg-blue-100/20 rounded-lg"
-    ></div>
+    <div className="aspect-[5/7] border-2 border-blue-100/60 bg-blue-100/20 rounded-lg"></div>
   );
 }
