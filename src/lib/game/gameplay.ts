145aa6031167731f8743a75d5be7cc1f393cfe7e
Kristian Woolhouse <kwoolhouse@wonde.com>
2025-09-22 00:45:30 +0100

    Notes for how gameplay utilities might work

---
+++ src/lib/game/gameplay.ts
@@ -0,0 +1,42 @@
+export function moveCard() {
+  // TODO
+  // Is the card the start of a sub-stack?
+  // --> Is there a chain of cards below it?
+  // --> Is there anywhere to move the sub-stack to?
+  // --> Are there enough free cells and open cells to move the sub-stack?
+  // if (cardIsStartOfSubStack()) {
+  //   const availableSpotForSubStack = getSpotForSubStack();
+  //   if (availableSpotForSubStack && canMoveSubStack()) {
+  //     moveSubStack();
+  //     return;
+  //   } else {
+  //     shakeSubStack();
+  //     return;
+  //   }
+  // }
+  // Is the card movable at all?
+  // --> Can it go on a foundation?
+  // --> Can it be placed at the bottom of a sub-stack?
+  // --> Are there any open cells?
+  // --> Are there any available free cells?
+  // if (canMoveToFoundation()) {
+  //   // Move card to foundation
+  // }
+  //
+  // if (canMoveToBottomOfSubStack()) {
+  //   // Move card to bottom of sub-stack
+  // }
+  //
+  // if (canMoveToOpenCell()) {
+  //   // Move card to open cell
+  // }
+  //
+  // if (canMoveToFreeCell()) {
+  //   // Move card to free cell
+  // }
+  //
+  // shakeCard();
+}
+
+// TODO
+// How can I update the state of tableau, freecells & foundations
