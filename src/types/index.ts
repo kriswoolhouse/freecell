4dfa2d43ec39689e61b46774de752fd7613812da
Kristian Woolhouse <kwoolhouse@wonde.com>
2025-09-23 01:20:45 +0100

    Map rank to value

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,6 +22,22 @@ export type Rank =
   | "Q"
   | "K";
 
+export const RANK_VALUES: { [key in Rank]: number } = {
+  A: 1,
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "10": 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+};
+
 export const suitSymbol: Record<Suit, string> = {
   hearts: "♥",
   diamonds: "♦",
